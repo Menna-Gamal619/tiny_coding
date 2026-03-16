616c8d32a077a57410bd37169ebe4ad3ffaa1187
NadaMousaa <nada.ahmedd.mousa@gmail.com>
2026-03-16 20:33:32 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace smartscanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            scan = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(477, 34);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(390, 393);
+            dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(78, 34);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(379, 393);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "int x := 10;\r\nfloat y := 3.14;\r\nwrite \"Hello\";\r\nif x > 5 && y < 10 then\r\n    x := x + 1;\r\nend\r\n";
+            // 
+            // scan
+            // 
+            scan.BackColor = Color.White;
+            scan.FlatStyle = FlatStyle.Flat;
+            scan.Font = new Font("Sitka Display", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            scan.Location = new Point(316, 472);
+            scan.Name = "scan";
+            scan.Size = new Size(299, 66);
+            scan.TabIndex = 2;
+            scan.Text = "scan";
+            scan.UseVisualStyleBackColor = false;
+            scan.Click += scan_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(961, 582);
+            Controls.Add(scan);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private Button scan;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System.Data;
+using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace smartscanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void scan_Click(object sender, EventArgs e)
+        {
+
+            string inputString = textBox1.Text;
+
+            string keywords = @"\b(int|float|string|read|write|repeat|until|if|elseif|else|then|return|endl)\b";
+            string identifiers = @"\b[a-zA-Z][a-zA-Z0-9_]*\b";
+            string numbers = @"\b[0-9]+\.[0-9]+|\b[0-9]+\b"; 
+            string strings = @"\""[^""]*\""";
+            string operators = @":=|==|!=|<=|>=|&&|\|\||[+\-*/<>]";  
+            string symbols = @"[,;(){}]";
+            string comments = @"/\*.*?\*/|//.*";  
+            string whitespace = @"[ \t\n\r]+";
+
+            string masterPattern = $"{comments}|{keywords}|{numbers}|{identifiers}|{strings}|{operators}|{symbols}|{whitespace}";
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Lexeme");
+            dt.Columns.Add("Token Type");
+
+            MatchCollection tokens = Regex.Matches(inputString, masterPattern);
+
+            foreach (Match token in tokens)
+            {
+                string lex = token.Value;
+
+                if (Regex.IsMatch(lex, comments) || Regex.IsMatch(lex, whitespace))
+                    continue;
+
+                string type = "";
+
+                if (Regex.IsMatch(lex, keywords))
+                    type = "Keyword";
+                else if (Regex.IsMatch(lex, @"^\d+\.\d+$")) 
+                    type = "Number Float";
+                else if (Regex.IsMatch(lex, @"^\d+$")) 
+                    type = "Number Integer";
+                else if (Regex.IsMatch(lex, strings))
+                    type = "String";
+                else if (Regex.IsMatch(lex, identifiers))
+                    type = "Identifier";
+
+                else if (lex == ":=") type = "Assign_Op";
+                else if (lex == "==") type = "Equal_Op";
+                else if (lex == "!=") type = "NotEqual_Op";
+                else if (lex == "<=") type = "LessEqual_Op";
+                else if (lex == ">=") type = "GreaterEqual_Op";
+                else if (lex == "<") type = "Less_Op";
+                else if (lex == ">") type = "Greater_Op";
+                else if (lex == "&&") type = "And_Op";
+                else if (lex == "||") type = "Or_Op";
+                else if (lex == "+") type = "Plus_Op";
+                else if (lex == "-") type = "Minus_Op";
+                else if (lex == "*") type = "Mult_Op";
+                else if (lex == "/") type = "Div_Op";
+                else if (lex == ",") type = "Comma";
+                else if (lex == ";") type = "Semicolon";
+                else if (lex == "(") type = "Left_Paren";
+                else if (lex == ")") type = "Right_Paren";
+                else if (lex == "{") type = "Left_Brace";
+                else if (lex == "}") type = "Right_Brace";
+
+                else type = "Unknown";
+
+                dt.Rows.Add(lex, type);
+
+
+            }
+
+            dataGridView1.DataSource = dt;
+            MessageBox.Show($"{dt.Rows.Count} Tokens Found!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    
+    }
+}
