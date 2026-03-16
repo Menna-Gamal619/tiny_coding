ebb353d2913f8b210bfd24e82fb85f451cad0e45
Menna-Gamal619 <mennagamal774@gmail.com>
2026-03-16 21:03:45 +0200

    if & comment_stmt & ...

--- Form1.cs
+++ Form1.cs
@@ -23,11 +23,18 @@ namespace compilerProject
             string userInput = textBox1.Text;
             string keywords = @"\b(num|text|check|otherwise|until|repeat)";
             string identifier = @"\b[a-zA-Z][a-zA-Z0-9]*\b";
-            string numbers = @"\b[0-9]+\b";
+            string numberPattern = @"\d+(\.\d+)?";
             string operators = @":=|==|[+\-*/<>]";
             string symbols = @"[;{}()]";
+            string comment_stmt = @"\b/*[a-zA-Z]*/|/*[0-9]*[a-zA-Z]*/\b";
+            string condition_op = @"<|>|=|<>|<=|>=";
+            string term = @"(\d+(\.\d+)?|[a-zA-Z_][a-zA-Z0-9_]*)";
+            string conditionPattern = $@"^{identifier}\s*{condition_op}\s*{term}$";
+            string bool_op = @"\s+($$||||)\s+";
+            string condition = $@"{identifier}*\s*{condition_op}\s*([a-zA-Z_][a-zA-Z0-9_]*|{numberPattern})";
+            string ifStatement = $@"^\s*if\s+{conditionPattern}\s+then\s+.*?(else\s+if|else|end)";
 
-            string masterPattern = $"{keywords}|{identifier}|{numbers}|{operators}|{symbols}";
+            string masterPattern = $"{keywords}|{identifier}|{numberPattern}|{operators}|{symbols}";
             DataTable dt=new DataTable();
             dt.Columns.Add("lexemes");
             dt.Columns.Add("Token type");
@@ -40,9 +47,15 @@ namespace compilerProject
                 string type = "";
 
                 if (Regex.IsMatch(lexeme, keywords))type = "keyword";
-                else if (Regex.IsMatch(lexeme, numbers))type = "numbers";
-                else if (Regex.IsMatch(lexeme, identifier))type = "identifier";
-                else if (Regex.IsMatch(lexeme, operators))type = "operators";
+                else if (Regex.IsMatch(lexeme, $@"^{ifStatement}$")) type = "If_Statement_Header";
+                else if (Regex.IsMatch(lexeme, $@"^{conditionPattern}$") || lexeme.Contains("&&") || lexeme.Contains("||")) type = "Condition_Statement";
+                else if (Regex.IsMatch(lexeme, @"\b(if|then|else|elseif|end)\b")) type = "Reserved_Keyword";
+                else if (Regex.IsMatch(lexeme, @"\b(num|text|check|otherwise|until|repeat)\b")) type = "Keyword";
+                else if (Regex.IsMatch(lexeme, $@"^{bool_op}$")) type = "Boolean_Operator";
+                else if (Regex.IsMatch(lexeme, $@"^{condition_op}$")) type = "Condition_Operator";
+                else if (Regex.IsMatch(lexeme, numberPattern)) type = "numbers";
+                else if (Regex.IsMatch(lexeme, identifier)) type = "identifier";
+                else if (Regex.IsMatch(lexeme, operators)) type = "operators";
                 else if (Regex.IsMatch(lexeme, symbols)) type = "symbols";
                 else if (lexeme == ":=") type = "Assignment_operator";
                 else if (lexeme == "==") type = "Equal_operator";
